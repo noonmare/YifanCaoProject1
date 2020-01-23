4582b781e53f9ef44df19ed7453168a3fe260bb7
noonmare <y4cao@student.bridgew.edu>
2020-01-23 11:50:58 -0500

    succeeding to synchronize with GitHub

---
+++ first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String[] args){
+        // example comment
+        System.out.println("Hello world");
+        System.out.println("Let's make it work");
+    }
+}
