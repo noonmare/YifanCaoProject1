eb270e624b260430ebb7bd70be204459fbb0abff
noonmare <y4cao@student.bridgew.edu>
2020-01-28 15:44:29 -0500

    succeeding to synchronize with GitHub

---
+++ Day2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args){
+        var reader = new Scanner(System.in);
+        System.out.print("What is your name?");
+        var name = reader.nextLine();
+        System.out.print("Hello"+" "+name);
+    }
+}
+
+// //
+/* */
