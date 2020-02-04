914effa9a2307d7858886b7adde44c4579001102
noonmare <y4cao@student.bridgew.edu>
2020-02-04 11:15:06 -0500

    succeeding to synchronize with GitHub

---
+++ Feb4.java
@@ -0,0 +1,7 @@
+public class Feb4 {
+    public static void main(String[] args) {
+        String[] data = {"12", "20", "30", "40"};
+        var example = data[0] + data[3];
+        System.out.println(example);
+    }
+}
