878c168633b0d0ac33c5854630c30be41e0b2107
noonmare <y4cao@student.bridgew.edu>
2020-02-04 13:53:00 -0500

    succeeding to synchronize with GitHub

--- Feb4.java
+++ Feb4.java
@@ -1,3 +1,4 @@
+//the first one treat number as the  data sets, but the second one convert the value as a number.
 public class Feb4 {
     public static void main(String[] args) {
         String[] data = {"12", "20", "30", "40"};
@@ -5,3 +6,18 @@ public class Feb4 {
         System.out.println(example);
     }
 }
+
+public class Feb4 {
+    public static void main(String[] args) {
+        String[] data = {"12", "20", "30", "40"};
+        var example =Integer.parseInt(data[0]) + Integer.parseInt(data[3]);
+        System.out.println(example);
+    }
+}
+
+
+/*What do I need for programming a bank account.
+Data: balance, customer, interest rate, account Type
+
+Operation: withdraw, deposit, transfer, make payment, report History. pay penality.
+ */
